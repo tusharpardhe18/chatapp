c653236c2cf9c0acee58617fe66850dccc63f7f5
tushar <tusharpardhe2018@gmail.com>
2025-08-25 11:22:21 +0530

    Feature: Create SignUp Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,19 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Settings } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { authUser, logout } = useAuthStore();
+  return (
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <Link
+            to="/"
+            className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+          >
+            <div className="flex items-center justify-center">
+              <img
+                width="94"
+                height="94"
+                src="https://img.icons8.com/3d-fluency/94/chat.png"
+                alt="chat"
+                className="w-12 h-12"
+              />
+            </div>
+            <h1 className="text-lg font-bold">Chatty</h1>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-2 ">
+          <Link
+            to={"/settings"}
+            className={`btn btn-sm gap-2 transition-colors`}
+          >
+            <Settings className="w-4 h-4" />
+            <span className="hidden sm:inline">Settings</span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
