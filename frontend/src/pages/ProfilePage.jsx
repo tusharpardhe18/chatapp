3ca984479f41bed545c21b505bd9f58320baf4a0
tushar <tusharpardhe2018@gmail.com>
2025-08-25 17:47:23 +0530

    Feature:  Added Profile Picture Page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,54 @@
+import { useAuthStore } from "../store/useAuthStore";
+import { Camera } from "lucide-react";
+// import {Camera}
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const handleImageUpload = async (e) => {};
+  return (
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold">Profile</h1>
+            <p className="mt-2">Your profile information</p>
+          </div>
+
+          {/**avatar upload section */}
+          <div className="flex flex-col items-center gap-4">
+            <div className="relative">
+              <img
+                src={authUser.profilePic || "/avatar.jpg"}
+                alt="Profile"
+                className="size-32 rounded-full object-cover border-4"
+              />
+              <label
+                htmlFor="avatar-upload"
+                className={`absolute bottom-0 right-0 bg-base-content hover:scale-105 p-2 rounded-full cursor-pointer transition-all duration-200 ${
+                  isUpdatingProfile ? "animate-pulse pointer-events-auto" : ""
+                }`}
+              >
+                <Camera className="w-5 h-5 text-base-200" />
+                <input
+                  type="file"
+                  id="avatar-upload"
+                  className="hidden"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                  disabled={isUpdatingProfile}
+                />
+              </label>
+            </div>
+            <p>
+              {isUpdatingProfile
+                ? "Uploading..."
+                : "Click the camera icon to update the your profile picture"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
