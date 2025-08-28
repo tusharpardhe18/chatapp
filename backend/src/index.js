c62c7c89fed755154be3fbeb7173a347212ac031
tushar <tusharpardhe2018@gmail.com>
2025-08-29 02:15:33 +0530

    Update : few updates like development and production mode

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 app.use(
@@ -24,6 +26,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT: " + PORT);
