402d7467fcce9f508319634d970413c26552cbfd
tushar <tusharpardhe2018@gmail.com>
2025-08-29 02:43:00 +0530

    Fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,42 @@
 import express from "express";
-import http from "http";
-import dotenv from "dotenv";
-import path from "path";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import { initSocket } from "./lib/socket.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+
+import path from "path";
 
 dotenv.config();
 
-const app = express();
-const server = http.createServer(app);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-// middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
-// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// serve frontend in production
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-// init socket
-initSocket(server);
-
-// start server
-const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log("Server running on PORT " + PORT);
+  console.log("Server is running on PORT: " + PORT);
   connectDB();
 });
